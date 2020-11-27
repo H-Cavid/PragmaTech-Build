7cb9d61a14200de279a0cc50eee7cab47c46de99
H-Cavid <cavid.hasanov2@bk.ru>
2020-11-28 03:55:43 +0400

    Update

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,19 @@
-from flask import render_template
+from flask import Flask, render_template, redirect, url_for
 from app import app
+
 @app.route("/")
 def Home():
-    return render_template("Home.html")#home page
+    return render_template("Home.html")#Home sehifesi
+
+@app.route("/Home")
+def Home1():
+    return render_template("Home.html")#Home sehifesi
+
+
+@app.route("/About")
+def About():
+   return render_template("About.html")#About Sehifesi
+
+@app.route("/Contact")
+def Contact():
+   return render_template("Contact.html")#About Sehifesi
