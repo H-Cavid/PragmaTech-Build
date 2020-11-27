6259b990cba45f33b63e2e7e7bd72be61b605f2e
H-Cavid <cavid.hasanov2@bk.ru>
2020-11-28 02:06:14 +0400

    Update Dashboard

--- admin/routes.py
+++ admin/routes.py
@@ -2,6 +2,6 @@ from flask import render_template
 from app import app
 
 @app.route("/admin/")
-def a_index():
-   return render_template("../templates/admin/index.html")
+def index():
+   return render_template("Layout.html")
 
